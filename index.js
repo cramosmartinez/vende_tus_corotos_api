251c6655c6ba70bb0368ee913eeb9f40d5b2dcc6
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-09 11:28:12 -0600

    Creamos nuestra base de datos para los productos. Decidimos cual es la estructura de un “Producto”. Escribimos la ruta para obtener a la colección de productos, y la ruta para obtener productos mediante su ID.

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { v4: uuid } = require('uuid');
 
 const app = express();
+//Base de datos de productos
 const productos = [
   { id: 1, nombre: "macbook pro 13", precio: 3000, moneda: "USD" },
   { id: 2, nombre: "taza de cafe", precio: 10, moneda: "USD" },
   { id: 3, nombre: "monitor", precio: 1000, moneda: "USD" },
 ];
 
-app.route("/productos").get((req, res) => {
-  res.json(productos);
-});
+app
+  .route("/productos")
+  .get((req, res) => {
+    res.json(productos);
+  })
+  //localhost:3000/productos
+  .post((req, res) => {
+    let nuevoProducto = req.body;
+    if (
+      !nuevoProducto.nombre ||
+      !nuevoProducto.precio ||
+      !nuevoProducto.moneda
+    ) {
+      //Bad request-No cumple tus requisitos
+      res.status(400).send("El producto debe tener nombre y precio.");
+      return;
+    }
+    nuevoProducto.id = uuid();
+    productos.push(nuevoProducto);
+    //Creado
+    res.status(201).json(nuevoProducto);
+  });
 
 app.get("/productos/:id", (req, res) => {
   for (let producto of productos) {

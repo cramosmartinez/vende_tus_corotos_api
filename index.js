50a1152527889980dfec736745331f87f53d8ceb
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-09 12:38:14 -0600

    Cada recurso va a tener su propia carpeta donde vamos a localizar todo el código relacionado a ese recurso (rutas, errores, validación, etc)

--- index.js
+++ index.js
@@ -1,88 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { v4: uuid } = require("uuid");
-const _ = require("underscore");
-const productos = require("./database").productos;
+const productosRouter = require("./api/recursos/productos/productos.routes");
 
 const app = express();
 app.use(bodyParser.json());
 //Base de datos de productos
 
-app
-  .route("/productos")
-  .get((req, res) => {
-    res.json(productos);
-  })
-  //localhost:3000/productos
-  //crear
-  .post((req, res) => {
-    let nuevoProducto = req.body;
-    if (
-      !nuevoProducto.nombre ||
-      !nuevoProducto.precio ||
-      !nuevoProducto.moneda
-    ) {
-      //Bad request-No cumple tus requisitos
-      res.status(400).send("El producto debe tener nombre y precio.");
-      return;
-    }
-    nuevoProducto.id = uuid();
-    productos.push(nuevoProducto);
-    //Creado
-    res.status(201).json(nuevoProducto);
-  });
-
-app
-  .route("/productos/:id")
-  //buscar
-  .get((req, res) => {
-    for (let producto of productos) {
-      if (producto.id == req.params.id) {
-        res.json(producto);
-        return;
-      }
-    }
-    //Not Found-Producto no encontrado
-    res.status(404).send("El producto con id " + req.params.id + " no existe.");
-  })
-  //actualizar
-  .put((req, res) => {
-    let id = req.params.id;
-    let remplazoParaProducto = req.body;
-    if (
-      !remplazoParaProducto.nombre ||
-      !remplazoParaProducto.precio ||
-      !remplazoParaProducto.moneda
-    ) {
-      //Bad request-No cumple tus requisitos
-      res.status(400).send("El producto debe tener nombre y precio.");
-      return;
-    }
-    let indice = _.findIndex(productos, (producto) => producto.id == id);
-    if (indice != -1) {
-      remplazoParaProducto.id = id;
-      productos[indice] = remplazoParaProducto;
-      res.status(200).json(remplazoParaProducto);
-    } else {
-      res
-        .status(404)
-        .send("El producto con id " + req.params.id + " no existe.");
-    }
-  })
-  //borrar
-  .delete((req, res) => {
-    let indiceBorrar = _.findIndex(
-      productos,
-      (producto) => producto.id == req.params.id
-    );
-    if (indiceBorrar === -1) {
-      res
-        .status(404)
-        .send("El producto con id " + req.params.id + " no existe.");
-    }
-    let borrado = productos.splice(indiceBorrar, 1);
-    res.status(200).json(borrado);
-  });
+app.use("/productos", productosRouter);
 
 //sevidor corriendo super basico
 app.get("/", (req, res) => {

f02d94e5c7a69a70db788e3e5048dfb0b8215755
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-10 12:18:56 -0600

    Integrar Morgan con Winston para Loggear Data de Requests

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const productosRouter = require("./api/recursos/productos/productos.routes");
 const logger = require("./utils/logger");
+const morgan = require("morgan");
+
 
 const app = express();
 app.use(bodyParser.json());
+app.use(morgan("short", {
+  stream: {
+    write: (message) => logger.info(message.trim()),
+  },
+}));
 
 //usamos el enrutador de productos
 app.use("/productos", productosRouter);

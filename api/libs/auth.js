dd3756a96139aef0c495a04170822caee0a487a7
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-11 20:33:08 -0600

    Remover credenciales 'hardcoded'. Verificar login usando DB.

--- api/libs/auth.js
+++ api/libs/auth.js
@@ -1,13 +1,28 @@
 const passport = require("passport");
 const log = require("../../utils/logger");
 const usuarios = require("../../database").usuarios;
-const bcrypt = require('bcryptjs');
-
+const bcrypt = require("bcryptjs");
+const _ = require("underscore");
 
 module.exports = (username, password, done) => {
-  if (username.valueOf() === "Carlos" && password.valueOf() === "1234") {
-    return done(null, true);
-  } else {
+  let index = _.findIndex(usuarios, (usuario) => {
+    return usuario.username === username;
+  });
+
+  if (index === -1) {
+    log.info(`Usuario ${username} no encontrado`);
     return done(null, false);
   }
+
+  let hashedPassword = usuarios[index].password;
+
+  bcrypt.compare(password, hashedPassword, (err, iguales) => {
+    if (iguales) {
+      log.info(`Usuario ${username} autenticado`);
+      return done(null, true);
+    } else {
+      log.info(`Contraseña incorrecta para el usuario ${username}`);
+      return done(null, false);
+    }
+  });
 };

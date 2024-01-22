6888a756e1d2694f6abb494c23959335a883d7b1
Carlos Ramos <cramos-2018521@kinal.edu.gt>
2024-01-22 14:36:27 -0600

    Terminar Tests a GET /usuarios

--- api/recursos/usuarios/usuarios.routes.test.js
+++ api/recursos/usuarios/usuarios.routes.test.js
@@ -2,6 +2,24 @@ let request = require("supertest");
 let Usuario = require("./usuarios.model");
 const { app, server } = require("../../../index");
 
+let dummyUsuarios = [
+  {
+    username: "daniel",
+    email: "daniel@gmail.com",
+    password: "holaquetal",
+  },
+  {
+    username: "ricardo",
+    email: "ricardo@gmail.com",
+    password: "quepaso",
+  },
+  {
+    username: "diego",
+    email: "diego@gmail.com",
+    password: "nomedigas",
+  },
+];
+
 describe("Usuarios", () => {
   beforeEach(async () => {
     await Usuario.deleteMany({});
@@ -23,6 +41,20 @@ describe("Usuarios", () => {
           expect(res.body).toHaveLength(0);
           done();
         });
+    });
+    test("Si existen usuarios, debería retornarlos en un array", (done) => {
+      Promise.all(
+        dummyUsuarios.map((usuario) => new Usuario(usuario).save())
+      ).then((usuarios) => {
+        request(app)
+          .get("/usuarios")
+          .end((err, res) => {
+            expect(res.status).toBe(200);
+            expect(res.body).toBeInstanceOf(Array);
+            expect(res.body).toHaveLength(3);
+            done();
+          });
+      });
     });
   });
 });
